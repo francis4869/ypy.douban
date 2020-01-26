2b9913fef85e21383da6872a3826dc8b050e5b76
Zlnc <478044574@qq.com>
2020-01-26 20:56:10 +0800

    change pages src

--- ypydouban/src/router.js
+++ ypydouban/src/router.js
@@ -3,11 +3,11 @@ import Vue from 'vue'
 
 
 
-import Home from './components/pages/Home'
-import Events from './components/pages/Events'
-import List from './components/pages/List'
-import Pger from './components/pages/Pger'
-import Video from './components/pages/Video.vue'
+import Home from './pages/Home'
+import Events from './pages/Events'
+import List from './pages/List'
+import Pger from './pages/Pger'
+import Video from './pages/Video.vue'
 
 
 Vue.use(VueRouter);
@@ -16,7 +16,7 @@ Vue.use(VueRouter);
 const router = new VueRouter({
     routes: [
         // 动态路径参数 以冒号开头
-        { path: '/index', component: Home },
+        { path: '/', component: Home },
         { path: '/Events', component: Events },
         { path: '/List', component: List },
         { path: '/Pger', component: Pger },
